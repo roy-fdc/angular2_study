5089a353af4f5b5f6be671c4c61d6ae5d8713594
johnyors <royfdc@gmail.com>
2016-07-13 09:59:43 +0800

    file up

---
+++ app/main.ts
@@ -0,0 +1,15 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
+
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS
+]);
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
